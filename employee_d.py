1cc07e31caadc3163963c436d6070486dfc93e5a
ritikakathare <ritikakathare15@gmail.com>
2025-11-20 15:34:04 +0530

    save

--- employee_d.py
+++ employee_d.py
@@ -1,14 +1,14 @@
 import sys
 if len(sys.argv) !=3:
-  print("usage: python employee_d.py<EMP_name> <salary> <id> <exp>")
+  print("usage: python employee_d.py<emp_name> <salary> <id> <exp>")
   sys.exit(1)
 script_name=sys.argv[0]
-EMP_name=sys.argv[1]
+emp_name=sys.argv[1]
 salary= sys.argv[2]
 id= sys.argv[3]
 exp=sys.argv[4]
 print("script name:",script_name)
-print("Employee name:",EMP_name)
+print("Employee name:",emp_name)
 print("Salary:",salary)
 print("ID:",id)
 print("expiriance:",exp)
